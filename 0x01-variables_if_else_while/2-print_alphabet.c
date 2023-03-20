0b5c84fa76aeb623358ead26f602a1ea21f5af56
harvey254 <harveybill74@gmail.com>
2023-03-20 20:36:55 +0000

    luck

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * prints the alphabet in lowercase
+ * Return: 0 Always (Success)
+ */
+lowercase(void)
+{
+for (int c = 97; c <= 122; ++c)
+putchar(c);
+}
+int main(void)
+{
+lowercase(void);
+
+return (0);
+}
+
