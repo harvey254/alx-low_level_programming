34c834ff82ac0f14d617eefeff5f4f41b7583d36
harvey254 <harveybill74@gmail.com>
2023-03-20 19:18:13 +0000

    luck

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive\n");
+printf("%d is positive\n");
 }
 else if (n == 0)
 {
-printf("is zero\n");
+printf("%d is zero\n");
 }
 else
 {
-printf("is negative\n");
+printf("% is negative\n");
 }
 return (0);
 }
