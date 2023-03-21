505ae45fb0849846588675a29590ac597b5e5dcf
harvey254 <harveybill74@gmail.com>
2023-03-21 23:01:05 +0000

    kj

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *@i -input integer
+ *_abs - computes absolute value
+ * Return: absolute value
+ */
+
+int _abs(int)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
+
