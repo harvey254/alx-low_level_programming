224fc721ebc20249d170001b523abc2891d1a0dd
harvey254 <harveybill74@gmail.com>
2023-03-21 20:36:40 +0000

    1st

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int _putchar(char c)
+{
+return (c);
+}
 void print_alphabet(void)
 {
 char letter = 'a';
@@ -12,4 +15,6 @@ letter++;
 }
 putchar('\n');
 }
+
+
 #endif 
