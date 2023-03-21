2e28f95ae997976b843fcec309670c3f7d872dc7
harvey254 <harveybill74@gmail.com>
2023-03-21 20:27:37 +0000

    1st

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,6 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
-{
-char letter = 'a';
-while (letter <= 'z')
-{
-putchar(letter);
-letter++;
-}
-putchar('\n');
-}
 
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
-{
-return (c);
-}
-
-
 
 void print_alphabet(void)
 {
