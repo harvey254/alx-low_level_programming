f2e48f4f71edbdb17d5abdde109db69cd5168ec8
harvey254 <harveybill74@gmail.com>
2023-03-21 20:21:19 +0000

    1st

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,22 @@
  * prints the alphabet in lowercase, followed by a new line
  * Return: Always 0 (Success)
  */
+int _putchar(char c)
+{
+return (c);
+}
+
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+
 int main(void)
 {
 print_alphabet();
