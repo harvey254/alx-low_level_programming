8fc2a3f4cc3459549bf803dfa6bc75dc5c3100b7
harvey254 <harveybill74@gmail.com>
2023-03-22 00:49:20 +0000

    kj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - prints natural numbers
  * @n: input integer
@@ -9,6 +10,6 @@ void print_to_98(int n)
 for (n = 0; n <= 98; n++)
 {
 printf("%d, ", n);
-
 }
+
 }
