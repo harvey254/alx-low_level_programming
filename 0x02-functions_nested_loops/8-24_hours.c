bf13c0a3ad4ee8d30bfa83a64c79cf0fcb52bc15
harvey254 <harveybill74@gmail.com>
2023-03-22 19:48:48 +0000

    zero

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute
+ * Return: no value
+ */
+
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+if (h < 10)
+{
+_putchar('0');
+}
+_putchar(h +'0');
+_putchar(':');
+if (m < 10)
+{
+_putchar('0');
+}
+_putchar(m + '0');
+_putchar('\n');
+}
+}
+}
+
