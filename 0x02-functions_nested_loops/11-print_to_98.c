6a315186cc5e86620719c4c9a3725edeb18c984d
harvey254 <harveybill74@gmail.com>
2023-03-22 00:58:54 +0000

    kj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,9 +7,27 @@
  */
 void print_to_98(int n)
 {
-for (n = 0; n <= 98; n++)
+if (n <= 98)
 {
-printf("%d, ", n);
+for (i = n; i <= 98; i++)
+{
+printf("%d, ", i);
+if (i != 98)
+{
+printf(", ");
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i !+ 98)
+{
+printf(", ");
+}
+}
+}
+}
+}
 }
 
-}
