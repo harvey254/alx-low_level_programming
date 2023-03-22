1f8c631569cee6520ac3e58bbe66cec6a3ede78e
harvey254 <harveybill74@gmail.com>
2023-03-22 00:14:19 +0000

    kj

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int _isalpha(int c);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int i, int n);
 void print_to_98(int n);
 int print_sign(int n);
 int _abs(int i);
