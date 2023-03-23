ab8e2fdaa7ea304370743d631e9d36ff8c388a8e
harvey254 <harveybill74@gmail.com>
2023-03-23 20:45:28 +0000

    coin

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line
+ * @n: number of times _ is printed
+ * Return: no value
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n);
+}
+for (int i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+
