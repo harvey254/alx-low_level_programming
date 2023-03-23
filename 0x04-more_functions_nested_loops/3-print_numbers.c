be63faa211a346a61c4de1ecf74b2dc40212888d
harvey254 <harveybill74@gmail.com>
2023-03-23 19:05:40 +0000

    coin

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0-9
+ * Return: no value
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++;)
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
