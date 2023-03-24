c0cdcbf1ea76de38158f4137a8f323fa9ca8fcbe
harvey254 <harveybill74@gmail.com>
2023-03-24 23:23:57 +0000

    pot

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,28 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_line - draw a straight line
- * @n: number of times underscore is printed
- * Description: Can only use _putchar to print
+ * print_line - entry point
+ *
+ * Description: Prints lines
+ *@n: number of lines
+ * Return: void
  */
+
 void print_line(int n)
 {
-	int c;
+	int num_lines;
 
-	c = 0;
-	while (c < n)
+	if (n <= 0)
 	{
-		_putchar('_');
-
-		c++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (num_lines = 1; num_lines <= n; num_lines++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
 
