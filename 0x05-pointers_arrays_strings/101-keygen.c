cd91fec2c5364f8ef397b7dca92f664042f3a418
harvey254 <harveybill74@gmail.com>
2023-03-28 09:10:10 +0000

    zero

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 6
+/**
+ * main - random password generator
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ0123456789@$!"
+		srand(time(NULL));
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % sizeof(charset)]
+	}
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
