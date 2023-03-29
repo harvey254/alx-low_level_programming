25e07c48ea34b9a8f8f8e65f461921790a827334
harvey254 <harveybill74@gmail.com>
2023-03-29 17:22:11 +0000

    zoo

---
+++ 0x06-pointers_arrays_strings/5-sring_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @str: string value in lowercase
+ * Return: Uppercase string value
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+	while (*ptr != '\0')
+	{
+		if (islower(*ptr))
+		{
+			*ptr = toupper(*ptr);
+		}
+		ptr++;
+	}
+	return (str);
+}
