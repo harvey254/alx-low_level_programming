c4603d4acac50cc46c426c260064d09b6b7038ae
harvey254 <harveybill74@gmail.com>
2023-03-29 16:52:14 +0000

    zoo

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string value
+ * @s2: string value
+ * Return: comparison of string values
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	while (s1[i] == s2[i])
+	{
+
+		if (s1[i] == '\0')
+		{
+
+			return 0;
+		}
+
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
