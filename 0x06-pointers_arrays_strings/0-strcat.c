6f9f4ee12ecae305d6450adf3d2ef865b2ef9b04
harvey254 <harveybill74@gmail.com>
2023-03-29 09:24:56 +0000

    zoo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,19 @@ char  *_strcat(char *dest, char *src)
 {
 	char *ptr = dest;
 
-	while(*ptr != '\0')
+	while (*ptr != '\0')
+
 	{
 		ptr++;
-	}
 
+	}
 	while (*src != '0')
 	{
+
 		*ptr++ = *src++;
+
 	}
 	*ptr = '\0';
-
 	return (dest);
 }
 
