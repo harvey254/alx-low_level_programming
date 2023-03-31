fc770ef69352d6df57fa48dd533a75c9289ebbb8
harvey254 <harveybill74@gmail.com>
2023-03-31 15:25:09 +0000

    hu

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -20,10 +20,9 @@ char *_strcat(char *dest, char *src)
 	{
 
 		dest[dest_len + i] = src[i];
-
-		dest[dest_len + i] = '\0';
-
-		return (dest);
-
 	}
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
 }
