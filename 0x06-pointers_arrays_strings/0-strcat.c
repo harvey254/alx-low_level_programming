20e1b325d32c7a098f36c1d9cd37b38224ed1ec8
harvey254 <harveybill74@gmail.com>
2023-03-31 16:51:36 +0000

    out

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,19 +14,19 @@ char *_strcat(char *dest, char *src)
 {
 	char *p = dest;
 
-	while (*p != '\0')
+	while (*dest != '\0')
 	{
-		p++;
+		dest++;
 	}
 
 	while (*src != '\0')
 	{
-		*p = *src;
-		p++;
+		*dest = *src;
+		dest++;
 		src++;
 	}
 
-	*p = '\0';
+	*dest = '\0';
 
-	return (dest);
+	return (p);
 }
