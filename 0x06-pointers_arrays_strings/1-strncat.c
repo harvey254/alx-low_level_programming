34659ba37c06524060120c403dbb9d830f5f51aa
harvey254 <harveybill74@gmail.com>
2023-03-31 17:05:03 +0000

    out

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,33 +2,44 @@
 #include <string.h>
 
 /**
- * *_strcat - See description
+ * _strcat - See description
  * @dest: char type string
  * @src: char type string
+ * @n: number of bytes
  * Description: Copy the string pointed to by pointer `src` to
  * the buffer pointed to by `dest`
  * Return: Pointer to `dest`
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
-        char *p = dest;
+	char *p = dest;
 
-        while (*dest != '\0')
-        {
-                dest++;
-        }
+	while (*dest != '\0')
 
-        while (*src != '\0' && n > 0)
-        {
-                *dest = *src;
-                dest++;
-                src++;
+	{
+
+		dest++;
+
+	}
+
+	while (*src != '\0' && n > 0)
+
+	{
+
+		*dest = *src;
+
+		dest++;
+
+		src++;
+
 		n--;
-        }
 
-        *dest = '\0';
+	}
 
-        return (p);
+
+	*dest = '\0';
+
+	return (p);
 }
 
