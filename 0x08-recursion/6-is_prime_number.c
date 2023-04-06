3cf610c2dab84e42e95b5774dc69082b4bb53ced
harvey254 <harveybill74@gmail.com>
2023-04-06 19:43:35 +0000

    loop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _sqrt_recursion - 
+ * is_prime_number - checks whether a number is prime 
  * @n: integer value
- * Return : square root
+ * Return : 1 if prime number, 0 otherwise
  */
 int is_prime_number(int n)
 {
@@ -16,11 +16,13 @@ int is_prime_number(int n)
         if (i * i > n)
         {
                 return (1);
+		i++;
         }
 
 	if (n % i == 0)
 	{
 		return (0);
+		i++;
 	}
 
 	return (is_prime_number(n));
