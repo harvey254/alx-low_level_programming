9643e7f6c0bffbba0afeaf4c7736b85cf89a0996
harvey254 <harveybill74@gmail.com>
2023-04-06 18:51:43 +0000

    loop

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,10 @@
 int _strlen_recursion(char *s)
 {
         if (*s == '\0')
-        {
-                _putchar('\n');
+	{
+	       	return (0);
 
-                return (0);
-        }
+	}
 
 	return (1 + _strlen_recursion(s + 1));
 }
