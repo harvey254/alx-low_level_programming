08fa89c04dc9fee7b086037600cd7614c1c2865c
harvey254 <harveybill74@gmail.com>
2023-04-06 19:09:34 +0000

    loop

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,42 +2,34 @@
 /**
  * _sqrt_recursion - calculates square root
  * @n: integer value
- * Return : square root
+ * Return: returns natural square root, otherwise it returns -1
  */
 
-int _sqrt_helper(int n, int l, int h)
-{
-	if (l > h)
-	{
-		return (-1);
-	}
-
-	int avg = (l + h) / 2;
-	int sq = avg * avg;
-
-	if (sq == n)
-	{
-		return (avg);
-	}	
-
-	else if (sq < n)
-	{
-		return (_sqrt_helper(n, avg + 1, h));
-	}	
-
-	else
-	{
-		return (_sqrt_helper(n, l, avg - 1));
-	}
-}
-
-int _sqrt_recursion(int n)
+int  _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
 
-	return (_sqrt_helper(n, 0, n));
+	else if (n == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		int i = _sqrt_recursion(n - 1) + 1;
+
+		if (i * i > n)
+		{
+			return (i - 1);
+		}
+
+		else
+		{
+			return (i);
+		}
+	}
 }
 
