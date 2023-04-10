a53ef8d95068d03d9f623e4914fbc0a4602ce2fb
harvey254 <harveybill74@gmail.com>
2023-04-10 17:15:37 +0000

    pool

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - a program that prints its name
+ * @argc: number of command line arguments
+ * @argv: pointer array
+ * Return: Always (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
+
