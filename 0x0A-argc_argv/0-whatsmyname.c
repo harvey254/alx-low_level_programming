e793b3dabb2589983f7b5bf67b8cffef77e130ad
harvey254 <harveybill74@gmail.com>
2023-04-10 16:47:40 +0000

    pool

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <libgen.h>
+/**
+ * main - a program that prints its name
+ * @argc: number of command line arguments
+ * @argv: pointer array
+ * Return: Always (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", basename(argv[0]));
+
+	return (0);
+}
