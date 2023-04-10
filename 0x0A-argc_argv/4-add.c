f657bba83dd4097bf907d7887230f1d91ddb209d
harvey254 <harveybill74@gmail.com>
2023-04-10 17:44:11 +0000

    pool

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: pointer array
+ * Return: Always (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("0\n");
+
+		return (0);
+	}
+
+	int i, j, res = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = arg[i];
+
+		for (int j = 0; arg[j] != '\0'; j++)
+		{
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n")
+
+					return (1);
+			}
+		}
+
+		res += atoi(arg);
+	}
+
+	printf("%d\n", res);
+
+	return (0);
+}
+
