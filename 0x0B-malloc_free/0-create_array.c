50f1b990b613f1124fe5f1909ef136f9ef051657
harvey254 <harveybill74@gmail.com>
2023-04-12 17:56:41 +0000

    jump

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,14 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *array;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *array = (char *) malloc(sizeof(char) * size);
+	*array = (char *) malloc(sizeof(char) * size);
 
 	if (array == NULL)
 	{
