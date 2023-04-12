2438a64d42a5336c1d5a9abbc69c4d4e18d9fcb7
harvey254 <harveybill74@gmail.com>
2023-04-12 18:30:14 +0000

    jump

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,33 +7,42 @@
  * Return:
  */
 
-int **alloc_grid(int width, int height) {
-    // If width or height is 0 or negative, return NULL
-    if (width <= 0 || height <= 0) {
-        return NULL;
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid = malloc(sizeof(int*) * height); 
+
+	if (width <= 0 || height <= 0)
+    {
+        return (NULL);
     }
 
-    // Allocate memory for the 2D array of integers
-    int **grid = (int**) malloc(sizeof(int*) * height);
-    if (grid == NULL) {
-        return NULL;
-    }
-    for (int i = 0; i < height; i++) {
-        grid[i] = (int*) malloc(sizeof(int) * width);
-        if (grid[i] == NULL) {
-            // Free previously allocated memory before returning NULL
-            for (int j = 0; j < i; j++) {
-                free(grid[j]);
-            }
-            free(grid);
-            return NULL;
-        }
-        // Initialize each element of the grid to 0
-        for (int j = 0; j < width; j++) {
-            grid[i][j] = 0;
-        }
+	if (grid == NULL)
+	{
+		return NULL;
+	}
+    for (i = 0; i < height; i++)
+    {
+	    grid[i] = malloc(sizeof(int) * width);
+
+	    if (grid[i] == NULL)
+	    {
+		    for (j = 0; j < i; j++)
+		    {
+			    free(grid[j]);
+		    }
+
+		    free(grid);
+
+		    return (NULL);
+	    }
+
+	    for (j = 0; j < width; j++)
+	    {
+		    grid[i][j] = 0;
+	    }
     }
 
-    return grid;
+    return (grid);
 }
 
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,16 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * free_grid - 
+ * free_grid - frees a 2D grid
  * @grid:
  * @height
  */
 
-void free_grid(int **grid, int height) {
-    // Free memory for each row of the 2D array
-    for (int i = 0; i < height; i++) {
-        free(grid[i]);
-    }
-
-    // Free memory for the array of integer pointers
-    free(grid);
+void free_grid(int **grid, int height)
+{
+	for (int i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
 }
 
