8fe1bb5ee39aa6437ad2f2e629c42b893e55ae93
harvey254 <harveybill74@gmail.com>
2023-04-17 14:34:03 +0000

    foo

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,15 +7,12 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		if (d->name)
-			free(d->name);
+	if (d->name)
+		free(d->name);
 
-		if (d->owner)
-			free(d->owner);
+	if (d->owner)
+		free(d->owner);
 
-		free(d);
-	}
+	free(d);
 
 }
