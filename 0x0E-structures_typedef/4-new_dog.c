f93f367ac6e13c3dcf44c5d1f7ad8cfbeb8f80d8
harvey254 <harveybill74@gmail.com>
2023-04-17 14:00:16 +0000

    foo

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,41 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
- * new_dog - creates a new dog
- * @name: name of new dog
- * @age: age of new dog
- * @owner: owner's name of new dog
- *
- * Return: new struct dog, NULL if it fails
- */
-
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog = (dog_t *) malloc(sizeof(dog_t));
+	int nlen, olen, i;
+	dog_t *doggy;
 
-    if (dog == NULL)
-	    return (NULL);
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
 
-    dog->name = strdup(name);
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
 
-    if (dog->name == NULL)
-    {
-	    free(dog);
+	doggy->age = age;
 
-	    return (NULL);
-    }
-
-    dog->age = age;
-    dog->owner = strdup(owner);
-
-    if (dog->owner == NULL)
-    {
-	    free(dog->name);
-	    free(dog);
-	    return (NULL);
-    }
-
-    return (dog);
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
-
