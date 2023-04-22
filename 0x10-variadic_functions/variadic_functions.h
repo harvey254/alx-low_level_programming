24ebacbea6521a3670fdbc1f99fcf1dafe9c9419
harvey254 <harveybill74@gmail.com>
2023-04-22 10:04:19 +0000

    alaji

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
