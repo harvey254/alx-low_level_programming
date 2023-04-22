74eb2d3189f15bad8f1698021fdda1400f5885b8
harvey254 <harveybill74@gmail.com>
2023-04-22 09:59:51 +0000

    alaji

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,15 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
+
 #endif
