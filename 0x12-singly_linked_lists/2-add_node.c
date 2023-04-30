3e388c5ebc666457440b079f3705a23296ec72e5
harvey254 <harveybill74@gmail.com>
2023-04-30 19:30:30 +0000

    kuy

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,10 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->next = *head;
-
-	*head = new_node;
-
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
@@ -31,6 +27,9 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+	new_node->next = *head;
+	*head = new_node;
+
 
 	return (new_node);
 }
