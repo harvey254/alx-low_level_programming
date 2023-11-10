02b0c474c7e55e4419eef4620cc0ecc46f233266
harvey254 <harveybill74@gmail.com>
2023-11-10 13:12:35 +0000

    mk

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <stddef.h>
-#include <ctype.h>
-void print_error_and_exit(int code, const char *message);
-void print_elf_header_info(Elf64_Ehdr *elf_header);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
