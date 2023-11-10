7f8014a3ba8eba574eafb965c163002896b3043f
harvey254 <harveybill74@gmail.com>
2023-11-10 10:32:56 +0000

    lk

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,12 +8,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 
-	if (filename == NULL| fd == -1)
+
+	if (filename == NULL | fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 		if (bytes_written == -1)
 		{
 			close(fd);
